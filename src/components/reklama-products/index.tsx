036241a217128dd9b4f9e128e2b9e62fe301c4db
DostonbekZaripov <dastandev0110@gmail.com>
2025-03-21 02:09:37 +0500

    reklama products edited

--- src/components/reklama-products/index.tsx
+++ src/components/reklama-products/index.tsx
@@ -6,27 +6,29 @@ const Reklama_Products = () => {
   return (
     <div className="py-5">
       <div className="containerr">
-        <div className="grid grid-cols-1  sm:grid-cols-2 gap-5">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-5">
           {info_item.map((value: InfoMockItemType) => (
             <div
               key={value.id}
-              className="flex  justify-between  items-center rounded-lg bg-[#FBFBFB] p-4 "
+              className="flex flex-col sm:flex-row items-center sm:items-center justify-center rounded-lg bg-[#FBFBFB] p-4 gap-4"
             >
               <img
                 src={value.img}
                 alt=""
-                className="max-w-[150px] md:max-w-[200px] lg:max-w-[287px] bg-transparent"
+                className="max-w-[120px] sm:max-w-[150px] md:max-w-[200px] lg:max-w-[287px] bg-transparent"
               />
-              <div className=" flex flex-col mr-5 gap-2 sm:gap-5 items-end text-end  w-full">
-                <div className="">
-                  <h3 className="max-w-[180px] font-bold text-[15px]  lg:text-[18px]">
-                    {value.title}
-                  </h3>
-                </div>
-                <p className="text-gray-400 text-[12px] md:text-[16px]  max-w-[300px]">
+
+              <div className="flex flex-col items-center sm:items-end text-center sm:text-end gap-2 sm:gap-4 w-full">
+                <h3 className="max-w-[250px] font-bold text-[14px] sm:text-[16px] lg:text-[18px]">
+                  {value.title}
+                </h3>
+                <p className="text-gray-400 text-[12px] sm:text-[14px] md:text-[16px] max-w-[300px]">
                   {value.description}
                 </p>
-                <Button type="primary" className="!bg-[#46A358]">
+                <Button
+                  type="primary"
+                  className="!bg-[#46A358] w-full sm:w-auto text-[12px] sm:text-[14px] md:text-[16px]"
+                >
                   Find More
                 </Button>
               </div>
