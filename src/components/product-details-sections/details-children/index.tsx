7538f08b7f194d489ad7fbcd6621d8e440f85711
DostonbekZaripov <dastandev0110@gmail.com>
2025-03-21 23:16:25 +0500

    product details pagega breadcrumb qoshildi

--- src/components/product-details-sections/details-children/index.tsx
+++ src/components/product-details-sections/details-children/index.tsx
@@ -1,9 +1,10 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import ProductDetailsInfos from "../details-info";
 import ProductDetailsItems from "../details-items";
 import ProductDetailsImgPreview from "../details-preview-img";
 import ReletedProducts from "../releted-products";
 import UseQueryHandler from "../../../hooks/useQuery";
+import { Breadcrumb } from "antd";
 
 const ProductDetailsChildren = () => {
   const { category, id }: { category?: string; id?: string } = useParams();
@@ -13,16 +14,35 @@ const ProductDetailsChildren = () => {
   });
   console.log(data);
 
-
   return (
-    <div className="containerr">
-      <div className="grid md:grid-cols-2 gap-10 py-5 sm:py-10">
-        <ProductDetailsImgPreview data={data} isPending={isPending} isError={isError} />
-        <ProductDetailsItems data={data} isPending={isPending} isError={isError} />
-      </div>
-      <ProductDetailsInfos data={data} />
-      <div>
-        <ReletedProducts />
+    <div className="py-5 sm:py-10">
+      <div className="containerr">
+        <Breadcrumb
+          items={[
+            {
+              title: <Link to={"/"}>Home</Link>,
+            },
+            {
+              title: "Product Preview",
+            },
+          ]}
+        />
+        <div className="grid md:grid-cols-2 gap-10 py-5 sm:py-10">
+          <ProductDetailsImgPreview
+            data={data}
+            isPending={isPending}
+            isError={isError}
+          />
+          <ProductDetailsItems
+            data={data}
+            isPending={isPending}
+            isError={isError}
+          />
+        </div>
+        <ProductDetailsInfos data={data} />
+        <div>
+          <ReletedProducts />
+        </div>
       </div>
     </div>
   );
