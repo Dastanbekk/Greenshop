4adbe994ea29e1cbc9aa9211ea8c21460d4ced4b
DostonbekZaripov <dastandev0110@gmail.com>
2025-03-27 01:32:01 +0500

    bugs fixed

--- src/components/blogs-children/blog-search/index.tsx
+++ src/components/blogs-children/blog-search/index.tsx
@@ -6,10 +6,10 @@ const BlogSearch = () => {
       <h3 className="font-bold text-[20px] sm:text-[26px] md:text-[30px] flex justify-center items-center">
         My Feed
       </h3>
-      <div className="w-[80%] mx-auto my-3">
+      <div className=" w-[95%] sm:w-[80%] mx-auto my-3">
         <form className="border-2 flex gap-1 border-[dodgerblue] rounded-lg">
           <input placeholder="Search text" type="text" className="bg-transparent px-2 w-full outline-none" />
-          <button className="bg-[dodgerblue] py-2 px-8">
+          <button className="bg-[dodgerblue]  py-1 sm:py-2 px-4 sm:px-8">
             <SearchOutlined className="!text-white !text-[18px]" />
           </button>
         </form>
--- src/components/blogs-children/index.tsx
+++ src/components/blogs-children/index.tsx
@@ -33,7 +33,7 @@ const BlogsChildren = () => {
       />
       {user ? <BlogSearch /> : <BlogHeader />}
 
-      <div className="grid grid-cols-3 gap-5">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
         {isPending || isError
           ? [1, 1, 1, 1, 1, 1].map((_, idx) => (
               <Card key={idx}>
