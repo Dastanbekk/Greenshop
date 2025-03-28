12238a7557b35e142753dbc7ac4603a1ec8c574e
DostonbekZaripov <dastandev0110@gmail.com>
2025-03-28 11:11:46 +0500

    bugs fixed

--- src/components/user-page-children/user-page-hero/index.tsx
+++ src/components/user-page-children/user-page-hero/index.tsx
@@ -1,7 +1,7 @@
 import { useParams } from "react-router-dom";
 import bgImg from "../../../assets/img/mountain-lake-header.webp";
 import UseQueryHandler from "../../../hooks/useQuery";
-import { Avatar, Button } from "antd";
+import { Avatar, Button, Skeleton } from "antd";
 import {
   MessageOutlined,
   PlusCircleOutlined,
@@ -10,10 +10,10 @@ import {
 
 const UserPageHero = () => {
   const param = useParams();
-  console.log(param.id);
+  let user_id = param.id;
 
-  const { data } = UseQueryHandler({
-    url: `user/by_id/${param}`,
+  const { data, isError, isPending } = UseQueryHandler({
+    url: `user/by_id/${user_id}`,
     pathname: "user-info",
   });
   console.log(data);
@@ -22,36 +22,57 @@ const UserPageHero = () => {
     <div className="py-5">
       <div className="containerr ">
         <div>
-          <img src={bgImg} className=" w-full min-h-[200px] object-cover" alt="" />
-          <div className="flex justify-between flex-wrap  gap-3 px-5 sm:mt-[-50px] items-end">
-            <div className="flex gap-2 items-center sm:items-end">
-              <Avatar
-                className="!min-w-[40px] !min-h-[40px]  !max-w-[100px] !w-full !h-full !max-h-[100px]"
-                src={data?.profile_photo}
-                alt=""
-              />
+          <img
+            src={bgImg}
+            className=" w-full min-h-[200px] object-cover"
+            alt=""
+          />
+          {isPending || isError ? (
+            <div className="flex items-end justify-between mt-5">
+              <div className="flex gap-3 items-end">
+              <Skeleton.Avatar />
               <div>
-                <h3 className="font-bold text-[18px]">
-                  {data?.name} {data?.surname}
-                </h3>
-                <p>Followers {data?.followers.length}</p>
+                <Skeleton.Button />
+                <Skeleton.Button />
+              </div>
+              </div>
+              <div className="flex gap-3">
+                <Skeleton.Button />
+                <Skeleton.Button />
+                <Skeleton.Button />
               </div>
             </div>
-            <div className="flex items-center gap-3">
-              <Button className="!bg-[#45a358] text-white" type="primary">
-                <MessageOutlined />
-                <p className="hidden sm:block">Start Chat</p>
-              </Button>
-              <Button className="!bg-[#45a358] text-white" type="primary">
-                <SendOutlined />
-                <p className="hidden sm:block">Send Invitation</p>
-              </Button>
-              <Button className="!bg-[#45a358] text-white" type="primary">
-                <PlusCircleOutlined />
-                <p className="hidden sm:block">Follow</p>
-              </Button>
+          ) : (
+            <div className="flex justify-between flex-wrap  gap-3 px-5 sm:mt-[-50px] items-end">
+              <div className="flex gap-2 items-center sm:items-end">
+                <Avatar
+                  className="!min-w-[40px] !min-h-[40px]  !max-w-[100px] !w-full !h-full !max-h-[100px]"
+                  src={data?.profile_photo}
+                  alt=""
+                />
+                <div>
+                  <h3 className="font-bold text-[18px]">
+                    {data?.name} {data?.surname}
+                  </h3>
+                  <p>Followers {data?.followers.length}</p>
+                </div>
+              </div>
+              <div className="flex items-center gap-3">
+                <Button className="!bg-[#45a358] text-white" type="primary">
+                  <MessageOutlined />
+                  <p className="hidden sm:block">Start Chat</p>
+                </Button>
+                <Button className="!bg-[#45a358] text-white" type="primary">
+                  <SendOutlined />
+                  <p className="hidden sm:block">Send Invitation</p>
+                </Button>
+                <Button className="!bg-[#45a358] text-white" type="primary">
+                  <PlusCircleOutlined />
+                  <p className="hidden sm:block">Follow</p>
+                </Button>
+              </div>
             </div>
-          </div>
+          )}
         </div>
       </div>
     </div>
